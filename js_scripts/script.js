36637c81d66c80363090dffe60473d81b52b0630
Luke Shanks <ls740@hubble.uscs.susx.ac.uk>
2024-01-23 14:26:35 +0000

    Added functionality for play/pause button.

---
+++ js_scripts/script.js
@@ -0,0 +1,29 @@
+var CURRENTSTATE    = "PAUSED" // states: "PAUSED" "STARTED"
+var CURRENTSTATUS   = "FOCUS"  // states: "WORK" & "BREAK"
+var CURRENTTIMELEFT = new Date(); 
+var WORKTIME        = new Date();
+var BREAKTIME       = new Date();
+var LONGBREAKTIME   = new Date();
+var LONGBREAKINTERVAL = 4; // how many breaks until a long break.
+var PERIODSPASSED   = 0;
+
+var main = function() {
+    var $clockStartLink = $("#clock_startOrPause")
+    var $clockReset     = $("#clock_reset")
+
+    $clockStartLink.on("click", function() {
+        if(CURRENTSTATE == "PAUSED") {
+            $clockStartLink.text("⏸︎")
+            CURRENTSTATE = "STARTED"
+        }
+
+        else if(CURRENTSTATE == "STARTED") {
+            $clockStartLink.text("⏵")
+            CURRENTSTATE = "PAUSED"
+        }
+
+        return false;
+    });
+}
+
+$(document).ready(main)
