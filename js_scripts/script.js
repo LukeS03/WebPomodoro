356bde54959e7145b7406db98226dd353a3f1e34
Luke Shanks <ls740@hubble.uscs.susx.ac.uk>
2024-01-23 16:01:41 +0000

    Wtf?

--- js_scripts/script.js
+++ js_scripts/script.js
@@ -1,19 +1,20 @@
-import "date-fns";
-
-var CURRENTSTATE    = "PAUSED" // states: "PAUSED" "STARTED"
-var CURRENTSTATUS   = "FOCUS"  // states: "WORK" & "BREAK"
-var CURRENTTIMELEFT = new Date(); 
-var WORKTIME        = new Date();
-var BREAKTIME       = new Date();
-var LONGBREAKTIME   = new Date();
-var LONGBREAKINTERVAL = 4; // how many breaks until a long break.
-var PERIODSPASSED   = 0;
+import {setMinutes, setSeconds, getMinutes, getSeconds} from 'date-fns'
 
 var main = function() {
+    var CURRENTSTATE    = "PAUSED" // states: "PAUSED" "STARTED"
+    var CURRENTSTATUS   = "FOCUS"  // states: "WORK" & "BREAK"
+    var CURRENTTIMELEFT = new Date("01-01-2001"); 
+    var WORKTIME        = new Date();
+    var BREAKTIME       = new Date();
+    var LONGBREAKTIME   = new Date();
+    var LONGBREAKINTERVAL = 4; // how many breaks until a long break.
+    var PERIODSPASSED   = 0;
+
+
     var $clockStartLink = $("#clock_startOrPause")
     var $clockReset     = $("#clock_reset")
 
-    WORKTIME = setMinutes(25);
+    WORKTIME = getMinutes(25);
     BREAKTIME = setMinutes(5);
     LONGBREAKTIME = setMinutes(15);
 
